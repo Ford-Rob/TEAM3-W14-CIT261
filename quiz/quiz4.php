46ea3d25071ef18837abd13ebd3bac26bad167e4
Ford-Rob <rob4rd@gmail.com>
2014-03-02 23:27:26 -0600

    Week 8 - Lesson 7

--- quiz/quiz4.php
+++ quiz/quiz4.php
@@ -9,10 +9,10 @@
 		  }
 
 		  #ccontainer{
-		  width:320px;
+		  width:450px;
 
 		  margin: 0 auto;
-		  margin-top:10px;
+		  margin-top:100px;
 		  }
 
 		  #myCanvas {
@@ -35,6 +35,7 @@
 				var Option1 = new String;
 				var Option2 = new String;
 				var Option3 = new String;
+				var Option4 = new String;
 				var mx=0;
 				var my=0;
 				var CorrectAnswer = 0;
@@ -45,8 +46,9 @@
 				var lock = false;
 				var textpos1=45;
 				var textpos2=145;
-				var textpos3=230;
+				var textpos3=235;
 				var textpos4=325;
+				var textpos5=410;
 				var Questions = new Array;
 				var Options = new Array;
 				
@@ -68,7 +70,8 @@
 					echo "\n";
 					echo "Options[".$i."]=['".$xml-> task[$i]->option[0] ."','";
 					echo $xml-> task[$i]->option[1] ."','";
-					echo $xml-> task[$i]->option[2]."'];";
+					echo $xml-> task[$i]->option[2]."','";
+					echo $xml-> task[$i]->option[3]."'];";
 					echo "\n";
 					}
 
@@ -82,18 +85,19 @@
 			      context.drawImage(quizbg, 0, 0);
 				  SetQuestions();
 				}//quizbg
-				quizbg.src = "quizbg.png";
+				quizbg.src = "quizbg4.png";
 
 
 
 				SetQuestions = function(){
 
 					Question=Questions[qnumber];
-					CorrectAnswer=1+Math.floor(Math.random()*3);
+					CorrectAnswer=1+Math.floor(Math.random()*4);
 
-					if(CorrectAnswer==1){Option1=Options[qnumber][0];Option2=Options[qnumber][1];Option3=Options[qnumber][2];}
-					if(CorrectAnswer==2){Option1=Options[qnumber][2];Option2=Options[qnumber][0];Option3=Options[qnumber][1];}
-					if(CorrectAnswer==3){Option1=Options[qnumber][1];Option2=Options[qnumber][2];Option3=Options[qnumber][0];}
+					if(CorrectAnswer==1){Option1=Options[qnumber][0];Option2=Options[qnumber][1];Option3=Options[qnumber][2];Option4=Options[qnumber][3];}
+					if(CorrectAnswer==2){Option1=Options[qnumber][2];Option2=Options[qnumber][0];Option3=Options[qnumber][3];Option4=Options[qnumber][1];}
+					if(CorrectAnswer==3){Option1=Options[qnumber][1];Option2=Options[qnumber][3];Option3=Options[qnumber][0];Option4=Options[qnumber][2];}
+					if(CorrectAnswer==4){Option1=Options[qnumber][3];Option2=Options[qnumber][2];Option3=Options[qnumber][1];Option4=Options[qnumber][0];}
 
 					context.textBaseline = "middle";
 					context.font = "24pt Calibri,Arial";
@@ -102,6 +106,7 @@
 					context.fillText(Option1,20,textpos2);
 					context.fillText(Option2,20,textpos3);
 					context.fillText(Option3,20,textpos4);
+					context.fillText(Option4,20,textpos5);
 
 
 				}//SetQuestions
@@ -127,6 +132,7 @@
 			if(my>110 && my<180){GetFeedback(1);}
 			if(my>200 && my<270){GetFeedback(2);}
 			if(my>290 && my<360){GetFeedback(3);}
+			if(my>380 && my<450){GetFeedback(4);}
 
 			}//!lock
 
@@ -137,23 +143,23 @@
 		GetFeedback = function(a){
 
 		  if(a==CorrectAnswer){
-		  	context.drawImage(quizbg, 0,400,75,70,240,110+(90*(a-1)),75,70);
+		  	context.drawImage(quizbg, 5,489,75,70,380,110+(90*(a-1)),75,70);
 			rightanswers++;
 			//drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
 		  }
 		  else{
-		    context.drawImage(quizbg, 75,400,75,70,240,110+(90*(a-1)),75,70);
+		    context.drawImage(quizbg, 80,489,75,70,380,110+(90*(a-1)),75,70);
 			wronganswers++;
 		  }
 		  lock=true;
 		  context.font = "14pt Calibri,Arial";
-		  context.fillText("Click again to continue",20,380);
+		  context.fillText("Click again to continue",20,465);
 		}//get feedback
 
 
 		ResetQ= function(){
 		lock=false;
-		context.clearRect(0,0,320,400);
+		context.clearRect(0,0,450,480);
 		qnumber++;
 		if(qnumber==Questions.length){EndQuiz();}
 		else{
@@ -164,9 +170,9 @@
 
 		EndQuiz=function(){
 		canvas.removeEventListener('click',ProcessClick,false);
-		context.drawImage(quizbg, 0,0,320,90,0,0,320,400);
+		context.drawImage(quizbg, 0,0,450,90,0,0,550,480);
 		context.font = "20pt Calibri,Arial";
-		context.fillText("You have finished the quiz!",10,100);
+		context.fillText("You have finished the quiz!",20,100);
 		context.font = "16pt Calibri,Arial";
 		context.fillText("Correct answers: "+String(rightanswers),20,200);
 		context.fillText("Wrong answers: "+String(wronganswers),20,240);
@@ -180,7 +186,7 @@
     <body>
 
     <div id="ccontainer">
-<canvas id="myCanvas" width="320" height="400"></canvas>
+<canvas id="myCanvas" width="450" height="480"></canvas>
         </div>
 
 
