e75b3f865add13ae674d4c049f0ed68704a44545
Ford-Rob <rob4rd@gmail.com>
2014-03-22 21:52:21 -0500

    Web Worker Timer

--- quiz/quiz4.php
+++ quiz/quiz4.php
@@ -27,7 +27,6 @@
 
         <script>
             window.onload = function(){
-
                 var canvas = document.getElementById("myCanvas");
                 var context = canvas.getContext("2d");
 				var quizbg = new Image();
@@ -82,9 +81,10 @@
 
 
 				quizbg.onload = function(){
-			      context.drawImage(quizbg, 0, 0);
-				  SetQuestions();
-				}//quizbg
+                                    startTimer();
+                                    context.drawImage(quizbg, 0, 0);
+                                    SetQuestions();
+                                    };//quizbg
 				quizbg.src = "quizbg4.png";
 
 
@@ -94,10 +94,10 @@
 					Question=Questions[qnumber];
 					CorrectAnswer=1+Math.floor(Math.random()*4);
 
-					if(CorrectAnswer==1){Option1=Options[qnumber][0];Option2=Options[qnumber][1];Option3=Options[qnumber][2];Option4=Options[qnumber][3];}
-					if(CorrectAnswer==2){Option1=Options[qnumber][2];Option2=Options[qnumber][0];Option3=Options[qnumber][3];Option4=Options[qnumber][1];}
-					if(CorrectAnswer==3){Option1=Options[qnumber][1];Option2=Options[qnumber][3];Option3=Options[qnumber][0];Option4=Options[qnumber][2];}
-					if(CorrectAnswer==4){Option1=Options[qnumber][3];Option2=Options[qnumber][2];Option3=Options[qnumber][1];Option4=Options[qnumber][0];}
+					if(CorrectAnswer===1){Option1=Options[qnumber][0];Option2=Options[qnumber][1];Option3=Options[qnumber][2];Option4=Options[qnumber][3];}
+					if(CorrectAnswer===2){Option1=Options[qnumber][2];Option2=Options[qnumber][0];Option3=Options[qnumber][3];Option4=Options[qnumber][1];}
+					if(CorrectAnswer===3){Option1=Options[qnumber][1];Option2=Options[qnumber][3];Option3=Options[qnumber][0];Option4=Options[qnumber][2];}
+					if(CorrectAnswer===4){Option1=Options[qnumber][3];Option2=Options[qnumber][2];Option3=Options[qnumber][1];Option4=Options[qnumber][0];}
 
 					context.textBaseline = "middle";
 					context.font = "24pt Calibri,Arial";
@@ -107,9 +107,9 @@
 					context.fillText(Option2,20,textpos3);
 					context.fillText(Option3,20,textpos4);
 					context.fillText(Option4,20,textpos5);
+                                        
 
-
-				}//SetQuestions
+				};//SetQuestions
 
 				canvas.addEventListener('click',ProcessClick,false);
 
@@ -118,7 +118,7 @@
 				mx=ev.x-canvas.offsetLeft;
 				my=ev.y-canvas.offsetTop;
 				
-				if(ev.x == undefined){
+				if(ev.x === undefined){
 					mx = ev.pageX - canvas.offsetLeft;
 					my = ev.pageY - canvas.offsetTop;
 				}
@@ -142,7 +142,7 @@
 
 		GetFeedback = function(a){
 
-		  if(a==CorrectAnswer){
+		  if(a===CorrectAnswer){
 		  	context.drawImage(quizbg, 5,489,75,70,380,110+(90*(a-1)),75,70);
 			rightanswers++;
 			//drawImage(image, sx, sy, sWidth, sHeight, dx, dy, dWidth, dHeight)
@@ -150,25 +150,43 @@
 		  else{
 		    context.drawImage(quizbg, 80,489,75,70,380,110+(90*(a-1)),75,70);
 			wronganswers++;
+                        if(CorrectAnswer===1){
+                            context.font = "18pt Calibri,Arial";
+                            context.fillStyle = 'green';
+                            context.fillText(Option1,20,textpos2);}
+			if(CorrectAnswer===2){                            
+                            context.font = "18pt Calibri,Arial";
+                            context.fillStyle = 'green';
+                            context.fillText(Option2,20,textpos3);}
+			if(CorrectAnswer===3){
+                            context.font = "18pt Calibri,Arial";
+                            context.fillStyle = 'green';
+                            context.fillText(Option3,20,textpos4);}                           
+			if(CorrectAnswer===4){
+                            context.font = "18pt Calibri,Arial";
+                            context.fillStyle = 'green';
+                            context.fillText(Option4,20,textpos5);}                                                   
 		  }
 		  lock=true;
 		  context.font = "14pt Calibri,Arial";
+                  context.fillStyle = 'black';
 		  context.fillText("Click again to continue",20,465);
-		}//get feedback
+		};//get feedback
 
 
 		ResetQ= function(){
 		lock=false;
 		context.clearRect(0,0,450,480);
 		qnumber++;
-		if(qnumber==Questions.length){EndQuiz();}
+		if(qnumber===Questions.length){EndQuiz();}
 		else{
 		context.drawImage(quizbg, 0, 0);
 		SetQuestions();}
-		}
+		};
 
 
 		EndQuiz=function(){
+                stopTimer();
 		canvas.removeEventListener('click',ProcessClick,false);
 		context.drawImage(quizbg, 0,0,450,90,0,0,550,480);
 		context.font = "20pt Calibri,Arial";
@@ -176,17 +194,33 @@
 		context.font = "16pt Calibri,Arial";
 		context.fillText("Correct answers: "+String(rightanswers),20,200);
 		context.fillText("Wrong answers: "+String(wronganswers),20,240);
-		}
+		};
+                startTimer=function(){
+                    if(typeof(Worker)!=="undefined") {
+                    if(typeof(w)==="undefined") {
+                        w=new Worker("worker.js");
+                        }
+                    w.onmessage = function (event) {
+                        document.getElementById("result").innerHTML=event.data;
+                        };
+                    } else {
+                        document.getElementById("result").innerHTML="Sorry, your browser does not support Web Workers...";
+                        }
+                    };
+                    stopTimer=function(){ 
+                        w.terminate();
+                        };
 			};//windowonload
 
         </script>
 
-
+        <title></title>
     </head>
     <body>
 
     <div id="ccontainer">
 <canvas id="myCanvas" width="450" height="480"></canvas>
+<p>time: <output id="result"></output></p>
         </div>
 
 
